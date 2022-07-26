fb289e62953e7f3b0a2eb6c9acfc56031b38c825
Stefano Calzoni <stefanoclz@hotmail.it>
2022-07-26 11:37:16 +0200

    Aggiutna chiamata API da PizzasController, inziio integrazione in schermata home

---
+++ Controllers/api/PizzasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.api
+{
+    [Route("Api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            PizzaContext db = new PizzaContext();
+
+            IQueryable<Pizza> pizzas = db.Pizza;
+
+            return Ok(pizzas.ToList());
+        }
+    }
+}
