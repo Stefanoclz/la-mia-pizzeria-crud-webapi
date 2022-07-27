204ed695b41e85bfa00184eb5d3a618538368c9b
Stefano Calzoni <stefanoclz@hotmail.it>
2022-07-27 14:22:11 +0200

    Inserito input per ricerca pizze e aggiornamento lista pizze

--- Controllers/api/PizzasController.cs
+++ Controllers/api/PizzasController.cs
@@ -8,13 +8,20 @@ namespace la_mia_pizzeria_static.Controllers.api
     public class PizzasController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string? search)
         {
             PizzaContext db = new PizzaContext();
 
             List<Pizza> pizzas = db.Pizza.ToList();
 
+            if (search != null && search != "")
+            {
+                pizzas = db.Pizza.Where(p => p.name.Contains(search)).ToList();
+            }
+
             return Ok(pizzas);
         }
+
+
     }
 }
